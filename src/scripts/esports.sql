e33d1335228130fa4853c6918ba2e0c2c8bef0c7
Paul Cox <pdcox64@gmail.com>
2020-02-27 16:07:04 -0400

    added assets

--- src/scripts/esports.sql
+++ src/scripts/esports.sql
@@ -15,22 +15,56 @@ INSERT INTO `couch_potato`.`teams` ( `name`, `icon`, `league` )
     VALUES ('Paul Cox','uk.png',17),
         ('Justin Cheung','canada.png',17),
         ('Adrian Metzler','germany.png',17),
-        ('Michael Seaward','canada.png',17);
+        ('Michael Seaward','canada.png',17),
+        ('Roshan Syed','canada.png',17),
+		('Sunny Singh','india.png',17),
+		('Manu Kumar','canada.png',17),
+		('Brady Groves','canada.png',17),
+		('Cory A.Ross','canada.png',17),
+		('Henry Belakon','belarus.png',17),
+		('Vikas Slathia','india.png',17),
+		('Eoin Whelan','ireland.png',17),
+        ('Jonathan Baha''i','canada.png',17),
+        ('Pravin Battu','india.png',17),
+        ('Bobinson Bobby','india.png',17),
+        ('Alick Felman','ukraine.png',17);
         
 /* Create Players */
 INSERT INTO `couch_potato`.`teams` ( `name`, `icon`, `league` )         
     VALUES ('Paul Cox','uk.png',18),
         ('Justin Cheung','canada.png',18),
         ('Adrian Metzler','germany.png',18),
-        ('Michael Seaward','canada.png',18);
+        ('Michael Seaward','canada.png',18),
+		('Roshan Syed','canada.png',18),
+		('Sunny Singh','india.png',18),
+		('Manu Kumar','canada.png',18),
+		('Brady Groves','canada.png',18),
+		('Cory A.Ross','canada.png',18),
+		('Henry Belakon','belarus.png',18),
+		('Vikas Slathia','india.png',18),
+		('Eoin Whelan','ireland.png',18),
+        ('Jonathan Baha''i','canada.png',18),
+        ('Pravin Battu','india.png',18),
+        ('Bobinson Bobby','india.png',18),
+        ('Alick Felman','ukraine.png',18);
+    
         
 /* Create Players */
 INSERT INTO `couch_potato`.`teams` ( `name`, `icon`, `league` )         
     VALUES ('Paul Cox','uk.png',19),
         ('Justin Cheung','canada.png',19),
         ('Adrian Metzler','germany.png',19),
-        ('Michael Seaward','canada.png',19);
-        
-
+        ('Michael Seaward','canada.png',19),
+        ('Roshan Syed','canada.png',19),
+		('Sunny Singh','india.png',19),
+		('Manu Kumar','india.png',19),
+		('Brady Groves','canada.png',19),
+		('Cory A.Ross','canada.png',19),
+		('Henry Belakon','belarus.png',19),
+		('Vikas Slathia','india.png',19),
+		('Eoin Whelan','ireleand.png',19),
+        ('Jonathan Baha''i','canada.png',19),
+        ('Pravin Battu','india.png',19),
+        ('Bobinson Bobby','india.png',19),
+        ('Alick Felman','ukraine.png',19);
     
-
